965508fa73986b668d62ac053a435ee1ac2f69d9
Dylan D <sovex66@gmail.com>
2016-01-22 20:32:04 +0100

    Initialisation du projet
    Ajout des du nécessaire pour débuter le projet

---
+++ includes/menu.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-default navbar-static-top">
+	<div class="container">
+		<div class="navbar-header">
+			<a href="index.php" class="navbar-brand">Knife</a>
+			<button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			</button>
+		</div>
+		<div class="navbar-collapse collapse" id="navbar-main">
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Features</a></li>
+				<li><a href="#">Portfolio</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
+		</div>
+	</div>
+</nav>
---
+++ sample.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Sample</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-flatly.css">
+</head>
+<body>
+	<!-- Menu de navigation -->
+	<?php include_once("includes/menu.php") ?>
+	<!-- Contenue de la page -->
+
+
+
+
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script src="js/jquery.js"></script>
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
